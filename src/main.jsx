ed0333c063126d06397203bc7600c694f1beca8d
oskcarsv <oskcarqsv@gmail.com>
2024-07-17 09:19:40 -0600

    change browser route in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <NextUIProvider>
       <App />
     </NextUIProvider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
