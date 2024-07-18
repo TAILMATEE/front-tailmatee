b103a977c02c84838d2f089abc817997c42c8d68
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-07-18 17:55:03 -0600

    Tailfriends added to user profile

--- src/pages/profile/profilePage.jsx
+++ src/pages/profile/profilePage.jsx
@@ -1,12 +1,14 @@
 import { NavHome } from '../../components/NavHome'
 import { ContentProfile } from '../../components/profile/ContentProfile'
+import { TailFriends } from '../../components/profile/TailFriends'
 
 export const ProfilePage = () => {
     return (
         <>
-            <main className='h-full'>
+            <main className='h-full flex flex-col items-center'>
                 <NavHome />
                 <ContentProfile />
+                <TailFriends />
             </main>
         </>
     )
