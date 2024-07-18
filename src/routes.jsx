27a0c17528ecea2444d87b4360d57203b5a3ca2e
oskcarsv <oskcarqsv@gmail.com>
2024-07-17 22:33:39 -0600

    create homepage route

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,10 +2,12 @@
 import { useRoutes } from "react-router-dom/dist";
 import { LoginPage } from "./pages/login/loginPage";
 import { RegisterPage } from "./pages/register/registerPage";
+import { HomePage } from "./pages/homepage/homePage";
 
 const routes = useRoutes([
   { path: '/', element: <LoginPage /> },
   { path: '/register', element: <RegisterPage /> }
+  { path: '/home', element: <HomePage /> }
 ]);
 
 export default routes;
