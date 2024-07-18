480f58134da42ed8717dbb21e539df1a028cb544
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-07-18 00:03:38 -0600

    Text from Login created

--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -1,9 +1,14 @@
 import { Nav } from "../../components/Nav.jsx";
+import { LoginText } from "../../components/login/LoginText/LoginText.jsx";
 
 export const LoginPage = () => {
   return (
-    <>
+    <main className="h-[70vh]">
       <Nav />
-    </>
+      <section className="flex flex-row justify-around items-center h-full">
+        <LoginText />
+        
+      </section>
+    </main>
   );
 };
