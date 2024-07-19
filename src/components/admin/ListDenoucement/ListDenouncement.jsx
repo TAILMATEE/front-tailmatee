c7feb547fc5150e0b9e9870274d5a4cc2f7f68aa
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-07-18 23:22:15 -0600

    Design from table updated

--- src/components/admin/ListDenoucement/ListDenouncement.jsx
+++ src/components/admin/ListDenoucement/ListDenouncement.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, getKeyValue } from "@nextui-org/react";
-import { EditIcon } from "../../../assets/Icons/AdminIcons/EditIcon.jsx";
+import AproveIcon  from "../../../assets/Icons/AdminIcons/AproveIcon.svg";
 import { EyeIcon } from "../../../assets/Icons/AdminIcons/EyeIcon.jsx";
 import { DeleteIcon } from "../../../assets/Icons/AdminIcons/DeleteIcon.jsx";
 import { columns, users } from "./data.js";
@@ -36,17 +36,17 @@ export const ListDenouncement = () => {
             case "opciones":
                 return (
                     <div className="relative flex items-center gap-2">
-                        <Tooltip content="Details" >
+                        <Tooltip content="Detalles" >
                             <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
                                 <EyeIcon />
                             </span>
                         </Tooltip>
-                        <Tooltip content="Edit user">
+                        <Tooltip content="Aprovar">
                             <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                                <EditIcon />
+                                <img src={AproveIcon} />
                             </span>
                         </Tooltip>
-                        <Tooltip color="danger" content="Delete user">
+                        <Tooltip color="danger" content="Eliminar!">
                             <span className="text-lg text-danger cursor-pointer active:opacity-50">
                                 <DeleteIcon />
                             </span>
