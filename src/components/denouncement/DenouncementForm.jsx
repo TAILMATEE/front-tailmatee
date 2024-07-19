d6ff729a927258cb740c029052304cdb29b4e459
oskcarsv <oskcarqsv@gmail.com>
2024-07-18 20:35:13 -0600

    finish denouncement

--- src/components/denouncement/DenouncementForm.jsx
+++ src/components/denouncement/DenouncementForm.jsx
@@ -1,38 +1,101 @@
-import { Accordion, AccordionItem } from "@nextui-org/react";
+import { DatePicker } from "@nextui-org/react";
+import { GeneralInput } from "../Input";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Button,
+  Textarea,
+} from "@nextui-org/react";
 
 export const DenouncementForm = () => {
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-
   return (
     <>
       <section className="p-20">
-        <Accordion variant="splitted">
-          <AccordionItem
-            key="1"
-            aria-label="Ubicación y lugar"
-            title="Ubicación y lugar"
-          >
-            {defaultContent}
-          </AccordionItem>
-          <AccordionItem
-            key="2"
-            aria-label="Tipo de mascota y de abuso"
-            title="Tipo de mascota y de abuso"
-          >
-            {defaultContent}
-          </AccordionItem>
-          <AccordionItem
-            key="3"
-            aria-label="Detalles de la denuncia"
-            title="Detalles de la denuncia"
-          >
-            {defaultContent}
-          </AccordionItem>
-        </Accordion>
-        <div>
-            
-        </div>
+        <Card className="w-auto h-full">
+          <CardHeader className="flex gap-3">
+            <div className="flex flex-col">
+              <p className="font-lato text-blue font-bold">
+                Fecha, hora y lugar del abuso
+              </p>
+              <p className="text-small text-default-500">Ingresa los datos</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <div className="flex flex-row flex-wrap gap-2 items-center justify-around mb-6">
+              <DatePicker
+                className="max-w-md"
+                granularity="second"
+                label="Fecha y hora del abuso"
+              />
+              <GeneralInput
+                type="text"
+                label="Ubicación exacta del abuso"
+                placeholder="Ingresa la dirección"
+                isRequired
+                className={"max-w-md"}
+                isClearable
+                onClear={() => console.log("Clear")}
+                color="default"
+              />
+            </div>
+            <div className="flex flex-row flex-wrap gap-2 items-center justify-around mb-6">
+              <GeneralInput
+                type="text"
+                label="Tipo de mascota"
+                placeholder="Perro, gato, etc."
+                isRequired
+                className={"max-w-md"}
+                isClearable
+                onClear={() => console.log("Clear")}
+                color="default"
+              />
+              <GeneralInput
+                type="text"
+                label="Tipo de abuso"
+                placeholder="Violencia, abandono, etc."
+                isRequired
+                className={"max-w-md"}
+                isClearable
+                onClear={() => console.log("Clear")}
+                color="default"
+              />
+            </div>
+
+            <div className="flex flex-row flex-wrap gap-2 items-center justify-around mb-6">
+              <Textarea
+                isRequired
+                label="Descripción del abuso"
+                labelPlacement="outside"
+                placeholder="Describe de manera detallada el abuso"
+                className="max-w-xl"
+              />
+            </div>
+            <div className="flex justify-center items-center">
+              <Button
+                radius="full"
+                className="max-w-xs mb-4 min-w-96 h-12"
+                color="warning"
+              >
+                Enviar denuncia
+              </Button>
+            </div>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <Link
+              isExternal
+              showAnchorIcon
+              href="https://www.worldanimalprotection.cr/apoyanos/actua-ahora/guia-denuncia-maltrato-animal/"
+            >
+              Guía de denuncia de maltrato animal
+            </Link>
+          </CardFooter>
+        </Card>
       </section>
     </>
   );
