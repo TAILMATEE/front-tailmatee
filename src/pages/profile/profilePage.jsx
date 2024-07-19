446d4cdd4b82d6103b395659f52cf52e841b572f
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-07-18 18:01:42 -0600

    Publications divider added

--- src/pages/profile/profilePage.jsx
+++ src/pages/profile/profilePage.jsx
@@ -1,14 +1,20 @@
 import { NavHome } from '../../components/NavHome'
 import { ContentProfile } from '../../components/profile/ContentProfile'
 import { TailFriends } from '../../components/profile/TailFriends'
+import { Divider } from "@nextui-org/divider";
 
 export const ProfilePage = () => {
     return (
         <>
-            <main className='h-full flex flex-col items-center'>
+            <main className='h-full flex flex-col items-center font-lato'>
                 <NavHome />
                 <ContentProfile />
                 <TailFriends />
+                <h1 className='text-2xl mt-5'>
+                    Publications
+                </h1>
+                <Divider className="my-4 max-w-7xl" />
+
             </main>
         </>
     )
